53a19eadaaf9a77141d79d29be26ea458cf8ce47
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-07-18 18:26:53 +0200

    feat: added main definition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import { Link } from "react-router-dom";
+import './App.css';
+import About from "./components/About";
+import Navigation from "./components/Navigation";
+import Vita from "./components/Vita";
+import { Link, Switch, Route } from 'react-router';
+import Publications from './components/Publications';
+import { Col, Container, Row } from 'react-bootstrap';
+import Interests from './components/Interests';
 function App() {
   return (
-    <div>
-      <Link to="/about">About</Link>
-      <h2>Github Pages</h2>
-      <h3>Deploying React to Github</h3>
-    </div>
+    <main>
+      <Navigation />
+      <Container>
+        <Row >
+          <Col xs={3}>
+            <About />
+          </Col>
+          <Col xs={8}>
+            <Vita />
+            <Publications />
+            <Interests />
+          </Col>
+        </Row>
+      </Container>
+
+    </main>
   );
 }
+
 export default App;
