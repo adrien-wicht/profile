de675c2374ec48c3c2fcba16815ee3911abd758c
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-07-19 18:07:35 +0200

    feat(categories): added research category

--- src/components/Research.js
+++ src/components/Research.js
@@ -6,6 +6,7 @@ import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import * as Icons from 'react-bootstrap-icons'
+import { Badge } from 'react-bootstrap';
 
 
 function BibTex(props) {
@@ -41,32 +42,73 @@ function BibTex(props) {
 export default function Research() {
 
 
+    // return (<section id="research">
+    //     <h1>Research <Button variant="link" onClick={() => { navigator.clipboard.writeText("https://adrien-wicht.github.io/profile/#research") }}><Icons.Link /></Button></h1>
+    //     {Pubs.map(pub =>
+    //         <>
+    //             <Card border="primary" class="Publication-card">
+    //                 <Card.Header><Badge>{pub.category}</Badge></Card.Header>
+    //                 <Card.Header>{pub.title} ({pub.issued['date-parts']}) <Button variant="link" href={pub.download}><Icon.FilePdf /></Button>  <BibTex pub={pub} /></Card.Header>
+    //                 <Card.Body>
+    //                     <blockquote className="blockquote mb-0">
+    //                         <p>
+    //                             {' '}
+    //                             {pub.abstract}
+    //                             {' '}
+    //                         </p>
+
+    //                         {pub.author.map(author => <a href={author.link}><footer className="text-muted">{author.family} {author.given}</footer></a>)}
+
+
+    //                     </blockquote>
+    //                 </Card.Body>
+    //             </Card>
+    //             <br></br>
+    //         </>
+
+
+    //     )}
+
+    // </section>)
+
+    const Types = [...new Set(Pubs.map(e => JSON.stringify(e.category)))].map(JSON.parse);
     return (<section id="research">
         <h1>Research <Button variant="link" onClick={() => { navigator.clipboard.writeText("https://adrien-wicht.github.io/profile/#research") }}><Icons.Link /></Button></h1>
-        {Pubs.map(pub =>
+
+        {Types.map(t =>
             <div>
+
                 <Card border="primary" class="Publication-card">
-                    <Card.Header>{pub.title} ({pub.issued['date-parts']}) <Button variant="link" href={pub.download}><Icon.FilePdf /></Button>  <BibTex pub={pub} /></Card.Header>
-                    <Card.Body>
-                        <blockquote className="blockquote mb-0">
-                            <p>
-                                {' '}
-                                {pub.abstract}
-                                {' '}
-                            </p>
+                    <Card.Header><Badge bg={t.colour}>{t.name}</Badge></Card.Header>
+                    <Card>
+                        {Pubs.filter(pub => pub.category.name === t.name).map(pub => {
+                            return <>
+                                <Card.Header>{pub.title} ({pub.issued['date-parts']}) <Button variant="link" href={pub.download}><Icon.FilePdf /></Button>  <BibTex pub={pub} /></Card.Header>
+                                <Card.Body>
+                                    <blockquote className="blockquote mb-0">
+                                        <p>
+                                            {' '}
+                                            {pub.abstract}
+                                            {' '}
+                                        </p>
 
-                            {pub.author.map(author => <a href={author.link}><footer className="text-muted">{author.family} {author.given}</footer></a>)}
+                                        {pub.author.map(author => <a href={author.link}><footer className="text-muted">{author.family} {author.given}</footer></a>)}
 
 
-                        </blockquote>
-                    </Card.Body>
-                    {/* <Card.Footer>Test</Card.Footer> */}
+                                    </blockquote>
+                                </Card.Body>
+                            </>
+                        })}
+                    </Card>
                 </Card>
                 <br></br>
             </div>
 
 
-        )}
+        )
 
-    </section>)
+        }
+
+
+    </section >)
 }
