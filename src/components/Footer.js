af541ee05f424362cd7a15b1b06c663dc0e318b3
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-07-19 16:57:54 +0200

    style: removed commented out code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,10 @@ export default function Footer() {
     return (
         <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
             <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-
-
-
             </section>
-
-
             <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
                 This website is totally open source. You can use, modify and redistribute it under the terms of the <a href="https://github.com/adrien-wicht/profile/blob/main/LICENSE">GPL license</a>.
-                {/* <a className='text-reset fw-bold' href='https://github.com/adrien-wicht/profile/blob/main/LICENSE'>
-                    GNU GENERAL PUBLIC LICENSE
-                </a> */}
+
             </div>
         </MDBFooter>
     );
