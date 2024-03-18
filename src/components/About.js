308aa9d0202d1824d2300ac8c3bad74abe25ea69
adrien-wicht <adrien.wicht@gmail.com>
2024-03-18 13:27:58 +0100

    institution update

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ function FaceCard() {
             </Card.Body>
 
             <Card.Footer>
-                <a href="https://www.eui.eu/people?id=adrien-wicht"><Icons.GeoAltFill /></a><a href="https://www.eui.eu/people?id=adrien-wicht">European University Institute</a>
+                <a href="https://wwz.unibas.ch/de/personen/adrien-wicht/"><Icons.GeoAltFill /></a><a href="https://wwz.unibas.ch/de/personen/adrien-wicht/">University of Basel</a>
                 <br />
                 <a href="mailto:adrien.wicht@unibas.ch"><Icons.EnvelopeFill /></a> <a href="mailto:adrien.wicht@unibas.ch"> adrien.wicht@unibas.ch</a>
                 <br />
